40646706bdb2d930cbf493e1df8a7e13d40df880
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-05-06 22:05:07 +0900

    posrgres_version_test

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ echo 'phpのテストです。<br>';
 
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 
-echo '<pre>'.var_dump($dbinfo).'</pre>';
+$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
+
+$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
+var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
 phpinfo();
 ?>
